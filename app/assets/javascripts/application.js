c522982d71d2ed4bf9f7e87c829d6bf0dbc5144a
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2015-01-06 15:40:33 -0500

    Get 100 records in the map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,25 +18,3 @@
 //= require heatmap
 //= require gmaps-heatmap
 //= require_tree .
-  
-function createSidebarLi(json){
-  return ("<li><a>" + json.name + "</a></li>");
-};
-
-function bindLiToMarker($li, marker){
-  $li.on('click', function(){
-    handler.getMap().setZoom(14);
-    marker.setMap(handler.getMap()); //because clusterer removes map property from marker
-    marker.panTo();
-    google.maps.event.trigger(marker.getServiceObject(), 'click');
-  })
-};
-
-function createSidebar(json_array){
-  _.each(json_array, function(json){
-    var $li = $( createSidebarLi(json) );
-    $li.appendTo('#sidebar_container');
-    bindLiToMarker($li, json.marker);
-  });
-};
-
