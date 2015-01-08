3906a82d0062f5bb706ae0da0bc005c183848cd0
Juan David Giraldo Rodriguez <jdgiraldo88@hotmail.com>
2015-01-08 11:06:02 -0500

    Populate data from heroku console

--- app/services/stack_overflow_search.rb
+++ app/services/stack_overflow_search.rb
@@ -25,4 +25,24 @@ class StackOverflowSearch
     end
     developers
   end
+
+  def self.stack_overflow_users
+    page = 1
+    begin
+      a = get_users(page, page)
+      
+      a.each do |user|
+        Developer.create(user_id: user[:user_id], name: user[:name], age: user[:age], location: user[:location],
+                         accept_rate: user[:accept_rate], website_url:  user[:website_url], link: user[:link], reputation: user[:reputation],
+                         reputation_change_year: user[:reputation_change_year], reputation_change_quarter: user[:reputation_change_quarter], 
+                         reputation_change_month: user[:reputation_change_month], reputation_change_week: user[:reputation_change_week], 
+                         bronze_badges: user[:bronze_badges], silver_badges: user[:silver_badges], gold_badges: user[:gold_badges],
+                         profile_image: user[:profile_image])
+      end
+
+      page +=1
+
+    end while page <= 100
+  end
+end
 end
